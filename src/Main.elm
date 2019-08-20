f7ff0c77de0d16970a450bf117f09c4a23f05425
Denis Rodionov <zunh@protonmail.com>
2019-08-20 23:38:14 +0300

    Loading and showing json data

--- src/Main.elm
+++ src/Main.elm
@@ -6,7 +6,8 @@ import Html
 import Html.Styled exposing (..)
 import Html.Styled.Attributes exposing (..)
 import Html.Styled.Events exposing (onClick)
-import Json.Decode exposing (Decoder, field, string)
+import Http
+import Json.Decode exposing (Decoder, field, list, map2, string)
 import TopList
 
 
@@ -17,29 +18,45 @@ type alias TopList =
     }
 
 
+type alias TopListItem =
+    { composer : String
+    , work : String
+    }
+
+
 type alias Model =
     { selectedListSlug : String
     , allTopLists :
         List TopList
+    , currentTopListItems : List TopListItem
     }
 
 
 type Msg
     = ClickedTopListSlug String
+    | GotJson (Result Http.Error (List TopListItem))
 
 
-initialModel : Model
-initialModel =
-    { selectedListSlug = "keyboard-concerti"
-    , allTopLists = TopList.lists
-    }
+initialModel : () -> ( Model, Cmd Msg )
+initialModel _ =
+    ( { selectedListSlug = "keyboard-concerti-100"
+      , allTopLists = TopList.lists
+      , currentTopListItems = []
+      }
+    , getTopListItems "keyboard-concerti-100"
+    )
 
 
-getListBySlug : String -> TopList
-getListBySlug slug =
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+getListBySlug : List TopList -> String -> TopList
+getListBySlug allTopLists slug =
     let
         list =
-            initialModel.allTopLists
+            allTopLists
                 |> List.filter (\l -> l.slug == slug)
                 |> List.head
     in
@@ -77,6 +94,31 @@ listElemFull topList =
         ]
 
 
+topListItem : TopListItem -> Html Msg
+topListItem item =
+    div [] [ text (item.composer ++ ": " ++ item.work) ]
+
+
+topListItemDecoder : Decoder TopListItem
+topListItemDecoder =
+    map2 TopListItem
+        (field "composer" string)
+        (field "work" string)
+
+
+topListItemsDecoder : Decoder (List TopListItem)
+topListItemsDecoder =
+    list topListItemDecoder
+
+
+getTopListItems : String -> Cmd Msg
+getTopListItems slug =
+    Http.get
+        { url = "/assets/json/" ++ slug ++ ".json"
+        , expect = Http.expectJson GotJson topListItemsDecoder
+        }
+
+
 view : Model -> Html Msg
 view model =
     let
@@ -87,20 +129,31 @@ view model =
         [ h1 [] [ text "All Lists" ]
         , ul [] (List.map (listElemShort model.selectedListSlug) topLists)
         , h1 [] [ text "Currently Selected List" ]
-        , listElemFull (getListBySlug model.selectedListSlug)
+        , listElemFull (getListBySlug model.allTopLists model.selectedListSlug)
+        , h1 [] [ text "Content" ]
+        , div [] (List.map topListItem model.currentTopListItems)
         ]
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         ClickedTopListSlug slugName ->
-            { model | selectedListSlug = slugName }
+            ( { model | selectedListSlug = slugName }, getTopListItems slugName )
+
+        GotJson result ->
+            case result of
+                Ok items ->
+                    ( { model | currentTopListItems = items }, Cmd.none )
+
+                Err _ ->
+                    ( model, Cmd.none )
 
 
 main =
-    Browser.sandbox
+    Browser.element
         { init = initialModel
-        , view = view >> toUnstyled
+        , subscriptions = subscriptions
         , update = update
+        , view = view >> toUnstyled
         }
--- src/TopList.elm
+++ src/TopList.elm
@@ -2,7 +2,7 @@ module TopList exposing (lists)
 
 
 lists =
-    [ { title = "Symphonies", description = "Top Symphonies", slug = "symphonies" }
-    , { title = "Keyboard Concerti", description = "Top Keyboard Concerti", slug = "keyboard-concerti" }
-    , { title = "Solo Keyboard", description = "Top Solo Keyboard", slug = "solo-keyboard" }
+    [ { title = "Symphonies", description = "Top Symphonies", slug = "symphonies-150" }
+    , { title = "Keyboard Concerti", description = "Top Keyboard Concerti", slug = "keyboard-concerti-100" }
+    , { title = "Solo Keyboard", description = "Top Solo Keyboard", slug = "solo-keyboard-200" }
     ]
